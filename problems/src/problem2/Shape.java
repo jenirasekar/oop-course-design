0c37134ab77642a83f4187eba9c7c79ad7500763
Jenira Sekar Az-zahra <jenirasekar@gmail.com>
2025-06-03 00:38:27 +0800

    problem 2, calculate shape's areas

---
+++ problems/src/problem2/Shape.java
@@ -0,0 +1,13 @@
+package problem2;
+
+public abstract class Shape {
+    public abstract double calculateArea();
+
+    public static void main(String[] args) {
+        Shape rectangle = new Rectangle(5, 3);
+        System.out.println("Rectangle area: " + rectangle.calculateArea());
+
+        Shape circle = new Circle(9);
+        System.out.println("Circle area: " + circle.calculateArea());
+    }
+}
